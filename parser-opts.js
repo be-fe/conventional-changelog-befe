562c27298257e8c2d420ec86797e757d4d02eddf
imcuttle <moyuyc95@gmail.com>
2018-08-27 20:56:14 +0800

    ci: test doc

--- parser-opts.js
+++ parser-opts.js
@@ -1,8 +1,8 @@
 'use strict'
 
 module.exports = {
-  // headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
-  headerPattern: /^([^:\s]*)(?:\((.*)\))?: (.*)$/,
+  headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
+  // headerPattern: /^([^:\s]*?)(?:\((.*)\))?: (.*)$/,
   headerCorrespondence: [`type`, `scope`, `subject`],
   noteKeywords: [`BREAKING CHANGE`],
   revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
