f9f4a6485f878a108c97934f2f486a68e5204278
--local <moyuyc95@gmail.com>
2018-10-23 18:00:24 +0800

    Revert "chore: add deprecated"
    This reverts commit ff55d57

--- deprecated.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file deprecated
- * @author Cuttle Cong
- * @date 2018/10/23
- *
- */
-
-console.log('[deprecated] @baidu/conventional-changelog-befe renames to conventional-changelog-befe in npmjs.com')
--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const parserOpts = require(`./parser-opts`)
 const recommendedBumpOpts = require(`./conventional-recommended-bump`)
 const writerOpts = require(`./writer-opts`)
 
-require('./deprecated')
-
 module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts]).spread(
   (conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
     return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts }
