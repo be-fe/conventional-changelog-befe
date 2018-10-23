ff55d5730221961c9a56f7222fff8ed7e51e35fe
--local <moyuyc95@gmail.com>
2018-10-23 17:59:12 +0800

    chore: add deprecated

---
+++ deprecated.js
@@ -0,0 +1,8 @@
+/**
+ * @file deprecated
+ * @author Cuttle Cong
+ * @date 2018/10/23
+ *
+ */
+
+console.log('[deprecated] @baidu/conventional-changelog-befe renames to conventional-changelog-befe in npmjs.com')
